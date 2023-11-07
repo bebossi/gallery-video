f3d5b72747d61f4c0be31ec2b444944326723f31
bebossi <bernardobossi77@gmail.com>
2023-11-07 09:42:32 -0300

    update this function to only create the videos of the playlist

--- src/app/api/playlistVideos/route.ts
+++ src/app/api/playlistVideos/route.ts
@@ -3,57 +3,96 @@ import axios from 'axios';
 import { NextApiRequest } from 'next';
 import { NextResponse } from 'next/server';
 
-export async function GET(req: NextApiRequest) {
-  const { searchParams } = new URL(req.url!);
+// export async function GET(req: NextApiRequest) {
+//   const { searchParams } = new URL(req.url!);
 
-  const playlistId = searchParams.get('playlistId') as string;
+//   const playlistId = searchParams.get('playlistId') as string;
+//   const url = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=25&playlistId=${playlistId}&key=${process.env.API_KEY_YOUTUBE}`;
+//   try {
+//     const response = await axios.get(url);
+//     const playlistItems = response.data.items;
+
+//     const playlist = await prismadb.playlist.findUnique({
+//       where: {
+//         id: playlistId,
+//       },
+//     });
+//     if (!playlist) {
+//       await prismadb.playlist.create({
+//         data: {
+//           id: playlistId,
+//         },
+//       });
+//     }
+
+//     for (const playlistItem of playlistItems) {
+//       const channelId = playlistItem.snippet.videoOwnerChannelId;
+//       const channelExists = await prismadb.channel.findUnique({
+//         where: {
+//           id: channelId,
+//         },
+//       });
+
+//       if (!channelExists) {
+//         await prismadb.channel.create({
+//           data: {
+//             id: channelId,
+//             title: playlistItem.snippet.videoOwnerChannelTitle,
+//             playlists: {
+//               connect: {
+//                 id: playlistId,
+//               },
+//             },
+//           },
+//         });
+//       }
+
+//       let video = await prismadb.video.findUnique({
+//         where: {
+//           id: playlistItem.contentDetails.videoId,
+//         },
+//       });
+//       if (!video) {
+//         await prismadb.video.create({
+//           data: {
+//             id: playlistItem.contentDetails.videoId,
+//             url: playlistItem.snippet.resourceId.videoId,
+//             title: playlistItem.snippet.title,
+//             description: playlistItem.snippet.description,
+//             thumbnailUrl: playlistItem.snippet.thumbnails.default.url,
+//             thumbnailWidth: playlistItem.snippet.thumbnails.default.width,
+//             thumbnailHeight: playlistItem.snippet.thumbnails.default.height,
+//             channelId,
+//             playlists: {
+//               connect: {
+//                 id: playlistId,
+//               },
+//             },
+//           },
+//         });
+//       }
+//     }
+//     // console.log(response.data);
+
+//     return NextResponse.json(response.data);
+//   } catch (err) {
+//     console.log(err);
+//     throw err;
+//   }
+// }
+export async function getVideosFromPlaylist(playlistId: string) {
   const url = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=25&playlistId=${playlistId}&key=${process.env.API_KEY_YOUTUBE}`;
   try {
     const response = await axios.get(url);
     const playlistItems = response.data.items;
-    console.log(response.data);
-
-    const playlist = await prismadb.playlist.findUnique({
-      where: {
-        id: playlistId,
-      },
-    });
-    if (!playlist) {
-      await prismadb.playlist.create({
-        data: {
-          id: playlistId,
-        },
-      });
-    }
 
     for (const playlistItem of playlistItems) {
-      const channelId = playlistItem.snippet.videoOwnerChannelId;
-      const channelExists = await prismadb.channel.findUnique({
-        where: {
-          id: channelId,
-        },
-      });
-
-      if (!channelExists) {
-        await prismadb.channel.create({
-          data: {
-            id: channelId,
-            title: playlistItem.snippet.videoOwnerChannelTitle,
-            playlists: {
-              connect: {
-                id: playlistId,
-              },
-            },
-          },
-        });
-      }
-
       let video = await prismadb.video.findUnique({
         where: {
           id: playlistItem.contentDetails.videoId,
         },
       });
-      // console.log(video);
+
       if (!video) {
         await prismadb.video.create({
           data: {
@@ -64,10 +103,20 @@ export async function GET(req: NextApiRequest) {
             thumbnailUrl: playlistItem.snippet.thumbnails.default.url,
             thumbnailWidth: playlistItem.snippet.thumbnails.default.width,
             thumbnailHeight: playlistItem.snippet.thumbnails.default.height,
-            channelId,
             playlists: {
               connect: {
                 id: playlistId,
+              },
+            },
+            channel: {
+              connectOrCreate: {
+                where: {
+                  id: playlistItem.snippet.videoOwnerChannelId,
+                },
+                create: {
+                  id: playlistItem.snippet.videoOwnerChannelId,
+                  title: playlistItem.snippet.videoOwnerChannelTitle,
+                },
               },
             },
           },
@@ -79,6 +128,6 @@ export async function GET(req: NextApiRequest) {
     return NextResponse.json(response.data);
   } catch (err) {
     console.log(err);
-    // throw err;
+    throw err;
   }
 }
