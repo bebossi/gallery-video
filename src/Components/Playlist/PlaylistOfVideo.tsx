fe5e198c4e2a65f7a98829d97c92beb125f30938
bebossi <bernardobossi77@gmail.com>
2023-11-09 09:25:53 -0300

    create PlaylistOfVideo component to display the videos of a playlist to show in the video page

---
+++ src/Components/Playlist/PlaylistOfVideo.tsx
@@ -0,0 +1,56 @@
+import { Playlist, Video, Channel } from '@prisma/client';
+import Link from 'next/link';
+import { AiFillCaretRight } from 'react-icons/ai';
+
+interface PlaylistOfVideoProps {
+  video: Video & {
+    channel: Channel;
+    playlists: (Playlist & {
+      videos: Video[];
+      channel: Channel[];
+    })[];
+  };
+  playlistId?: string;
+}
+
+const PlaylistOfVideo: React.FC<PlaylistOfVideoProps> = ({
+  video,
+  playlistId,
+}) => {
+  const playlist = video.playlists.find((playlist) => {
+    return playlist.id === playlistId;
+  });
+
+  return (
+    <div className="border-2 border-slate-300 rounded-xl  max-w-xl">
+      <div className="p-6 bg-slate-900">
+        <h1 className="font-bold text-lg">{playlist?.name}</h1>
+        <h2>{playlist?.channel[0].title}</h2>
+      </div>
+      <div>
+        {playlist?.videos.map((playlistVideo: Video) => (
+          <Link href={`/playlists/${playlistId}/${playlistVideo.id}`}>
+            <div
+              key={playlistVideo.id}
+              className={`${
+                video.id === playlistVideo.id ? 'bg-slate-900' : ''
+              }  flex items-center rounded-md p-1  gap-x-4`}
+            >
+              <p>
+                {video.id === playlistVideo.id ? (
+                  <AiFillCaretRight />
+                ) : (
+                  playlist.videos.indexOf(playlistVideo) + 1
+                )}
+              </p>
+              <img src={playlistVideo.thumbnailUrl!} />
+              <p className="line-clamp-1">{playlistVideo.title}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PlaylistOfVideo;
