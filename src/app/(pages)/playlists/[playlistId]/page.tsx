9649e87fde3dc0b6751a160b3000dc2b16ed1114
bebossi <bernardobossi77@gmail.com>
2023-11-14 15:10:30 -0300

    make the page responsive

--- src/app/(pages)/playlists/[playlistId]/page.tsx
+++ src/app/(pages)/playlists/[playlistId]/page.tsx
@@ -22,24 +22,23 @@ const PlaylistPage: React.FC<PlaylistPageProps> = async ({ params }) => {
       },
     },
   });
-  const createIframeElement = (html: any) => {
-    return {
-      __html: html,
-    };
-  };
+
   return (
-    <div className="flex justify-center gap-x-20">
+    <div className="flex flex-col justify-start items-start lg:flex-row xl:flex-row mt-6 ml-6 2xl:ml-56 mr-3">
       <PlaylistInfo playlist={playlist!} />
-      <div className="flex flex-col gap-6 mt-20">
+      <div className="flex flex-col w-full ">
         {playlist?.videos.map((video) => (
           <Link href={`/playlists/${playlistId}/${video.id}`}>
             <div
               key={video.id}
-              className="flex justify-start items-center gap-x-2 rounded-md p-2 hover:bg-slate-900 w-full"
+              className="flex justify-start items-start gap-x-2 rounded-md p-2 hover:bg-slate-900 w-full "
             >
-              <img className="h-[7rem] rounded-md" src={video.thumbnailUrl!} />
-              <div className="flex flex-col">
-                {video.title}
+              <img
+                className=" h-[4rem] sm:h-[5rem] md:h-[7rem] lg:h-[7rem] xl:h-[8rem] 2xl:h-[9rem] rounded-md"
+                src={video.thumbnailUrl!}
+              />
+              <div className="flex flex-col gap-y-4 text-sm md:text-base  lg:text-lg xl:text-xl  max-w-fit w-full ">
+                <p className="font-bold line-clamp-2">{video.title}</p>
                 <p>
                   {video.channel.title} . {video.viewCount} views
                 </p>
