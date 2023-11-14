5abb15f4c71dc26b6d2d583451d1e882bfbb0273
bebossi <bernardobossi77@gmail.com>
2023-11-14 15:08:25 -0300

    make components responsive

--- src/Components/Navbar/UserMenu.tsx
+++ src/Components/Navbar/UserMenu.tsx
@@ -13,42 +13,51 @@ const UserMenu = () => {
   }, []);
 
   return (
-    <div className="relative flex gap-x-12">
-      <div className="flex flex-row gap-x-4 items-center justify-center">
-        <MenuItem
-          label="Videos"
+    <div className="relative">
+      <div className="flex flex-row items-center gap-3">
+        <div
           onClick={() => {
             navigate.push('/videos');
           }}
-        />
-        <MenuItem
-          label="Playlists"
+          className="hidden lg:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
+        >
+          Videos
+        </div>
+        <div
           onClick={() => {
             navigate.push('/playlists');
           }}
-        />
-        <MenuItem
-          label="Channels"
+          className="hidden lg:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
+        >
+          Playlists
+        </div>
+        <div
           onClick={() => {
             navigate.push('/channels');
           }}
-        />
-        <MenuItem
-          label="Insert your playlist"
+          className="hidden lg:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
+        >
+          Channels
+        </div>
+        <div
           onClick={() => {
             navigate.push('/insertPlaylistId');
           }}
-        />
-      </div>
-      <div className="m-4 hover:cursor-pointer">
-        <AiOutlineMenu onClick={toggleOpen} size={25} />
+          className="hidden lg:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
+        >
+          Insert your playlist ID
+        </div>
+
+        <div className="m-4 hover:cursor-pointer">
+          <AiOutlineMenu onClick={toggleOpen} size={25} />
+        </div>
       </div>
       {isOpen && (
         <div className="absolute rounded-md shadow-md right-0 top-12 bg-gray-900 w-[12rem] flex flex-col items-start px-3 gap-y-2 ">
           <MenuItem label="Videos" onClick={() => {}} />
           <MenuItem label="Playlists" onClick={() => {}} />
           <MenuItem label="Channels" onClick={() => {}} />
-          <MenuItem label="Videos" onClick={() => {}} />
+          <MenuItem label="Insert your playlist ID" onClick={() => {}} />
         </div>
       )}
     </div>
--- src/Components/Playlist/PlaylistInfo.tsx
+++ src/Components/Playlist/PlaylistInfo.tsx
@@ -9,23 +9,31 @@ export interface PlaylistInfoProps {
 }
 const PlaylistInfo: React.FC<PlaylistInfoProps> = ({ playlist }) => {
   return (
-    <div className="flex flex-col items-center rounded-xl w-1/5 bg-slate-800 bg-opacity-60 mt-20">
-      <Link href={`/playlists/${playlist.id}/${playlist.videos[0].id}`}>
-        <div className="group relative">
+    <div className="flex flex-row items-center justify-start rounded-xl lg:flex-col h-full lg:h-screen w-screen sm:w-full md:w-full lg:w-1/2 xl:w-1/3 2xl:w-1/3 bg-slate-800 bg-opacity-80">
+      <Link
+        className="w-full"
+        href={`/playlists/${playlist.id}/${playlist.videos[0].id}`}
+      >
+        <div className="group relative flex flex-col items-center justify-center w-full">
           <img
-            className="h-48 w-5/6 rounded-sm my-3 group-hover:opacity-20"
+            className="h-40  lg:h-48 w-4/5 px-5 rounded-sm my-3 group-hover:opacity-20"
             src={playlist.thumbnailUrl!}
+            alt={`${playlist.name} thumbnail`}
           />
           <div className="hidden group-hover:block top-24 right-[8rem] text-2xl font-bold absolute rounded-sm text-white">
             Reproduce all
           </div>
         </div>
       </Link>
-      <div className="flex flex-col justify-start items-start w-full px-12">
-        <h1 className="font-bold text-2xl mb-6">{playlist.name}</h1>
-        <h2 className="font-semibold text-xl">{playlist.status}</h2>
-        <p>{playlist.videos.length} videos</p>
-        <p>{playlist.description}</p>
+      <div className="flex flex-col justify-start items-start w-full px-6 sm:px-12">
+        <h1 className="font-bold text-2xl sm:text-3xl mb-2 sm:mb-4">
+          {playlist.name}
+        </h1>
+        <h2 className="font-semibold text-xl sm:text-2xl">{playlist.status}</h2>
+        <p className="text-sm sm:text-base">{playlist.videos.length} videos</p>
+        <p className="text-sm sm:text-base">
+          {playlist.description || 'Playlist sem descrição'}
+        </p>
       </div>
     </div>
   );
