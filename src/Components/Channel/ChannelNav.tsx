299d56c55ca314d0d67e4fa5117878fe0edcabd9
bebossi <bernardobossi77@gmail.com>
2023-11-19 17:16:04 -0300

    add navigation bar to channel page

---
+++ src/Components/Channel/ChannelNav.tsx
@@ -0,0 +1,13 @@
+const ChannelNav = () => {
+  return (
+    <>
+      <ul className="flex gap-x-3 justify-center">
+        <li>Home</li>
+        <li>Videos</li>
+        <li>Playlists</li>
+      </ul>
+    </>
+  );
+};
+
+export default ChannelNav;
