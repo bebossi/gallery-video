98f3d2b036d291ed4d838dc63fa72a30a95642d4
bebossi <bernardobossi77@gmail.com>
2023-11-21 12:33:38 -0300

    update css in channel info component

--- src/Components/Channel/ChannelInfo.tsx
+++ src/Components/Channel/ChannelInfo.tsx
@@ -8,15 +8,17 @@ const ChannelInfo: React.FC<ChannelInfoProps> = ({ channel }) => {
   return (
     <div className="p-1">
       <div
-        className="bg-cover bg-center h-[5rem] sm:h-[9rem] md:h-[12rem] xl:h-[15rem] rounded-3xl mb-3"
+        className="bg-cover bg-center h-[10rem] w-auto lg:w-[40rem] xl:w-[50rem] 2xl:w-[70rem] rounded-3xl mb-3"
         style={{ backgroundImage: `url(${channel.image})` }}
       ></div>
-      <div className="flex items-center gap-x-[1rem] ">
-        <img
-          src={channel.thumbnailUrl!}
-          alt="image"
-          className="w-[6rem] h-[6rem] sm:h-[8rem] sm:w-[8rem] md:h-[10rem] md:w-[10rem] rounded-full"
-        />
+      <div className="flex items-center gap-x-[1rem] w-fit h-fit">
+        {channel.thumbnailUrl && (
+          <img
+            src={channel.thumbnailUrl!}
+            alt="image"
+            className="w-[6rem] h-[6rem] sm:h-[8rem] sm:w-[8rem] md:h-[10rem] md:w-[10rem] rounded-full"
+          />
+        )}
         <div className="flex flex-col gap-y-[0.5rem] ">
           <h1 className="text-xl sm:text-2xl md:text-4xl font-bold">
             {channel.title}
@@ -28,7 +30,7 @@ const ChannelInfo: React.FC<ChannelInfoProps> = ({ channel }) => {
             </span>{' '}
             {channel.videoCount} videos{' '}
           </h2>
-          <h3 className=" text-xs sm:text-sm md:text-lg  line-clamp-2">
+          <h3 className="text-xs sm:text-sm md:text-lg  line-clamp-2">
             {channel.description}
           </h3>
         </div>
