2dede27ee8135fa793afbe71c42ed90154436888
bebossi <bernardobossi77@gmail.com>
2023-11-25 07:50:59 -0300

    create component to display playlists

---
+++ src/Components/Playlist/DisplayPlaylists.tsx
@@ -0,0 +1,34 @@
+import { Channel, Playlist, Video } from '@prisma/client';
+import Link from 'next/link';
+
+interface DisplayPlaylistsProps {
+  playlist: Playlist & {
+    ownerChannel: Channel;
+    videos: Video[];
+  };
+}
+const DisplayPlaylists: React.FC<DisplayPlaylistsProps> = ({ playlist }) => {
+  return (
+    <div key={playlist.id} className="p-5 flex flex-row gap-x-3">
+      <div>
+        <img
+          className="  lg:w-[16rem] xl:w-[20rem] rounded-2xl "
+          src={playlist.thumbnailUrl!}
+        />
+      </div>
+      <Link href={`/playlists/${playlist.id}`}>
+        <div className="flex flex-col justify-start items-start  md:gap-y-1 lg:gap-y-2 lg:h-[10rem] w-[15rem] sm:w-[25rem] md:w-[30rem] lg:w-[35rem] xl:w-[40rem]">
+          <p className="text-2xl sm:text-xl">{playlist.name}</p>
+          <p>{playlist.ownerChannel.title}</p>
+          <div className="flex flex-col">
+            <p className="line-clamp-1 "> {playlist.videos[0].title} </p>
+            <p className="line-clamp-1 "> {playlist.videos[1].title}</p>
+          </div>
+          <p className="font-bold ">VIEW FULL PLAYLIST</p>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default DisplayPlaylists;
