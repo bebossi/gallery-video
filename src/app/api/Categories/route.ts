33c46eb8af05fde3a96600b2579bcce6a677a937
bebossi <bernardobossi77@gmail.com>
2023-11-26 15:36:30 -0300

    fetch youtube api url to store all categories in my database

---
+++ src/app/api/Categories/route.ts
@@ -0,0 +1,23 @@
+import { NextApiResponse } from 'next';
+import prismadb from '@/src/lib/prismadb';
+import axios from 'axios';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  const url = `https://youtube.googleapis.com/youtube/v3/videoCategories?part=snippet&regionCode=US&key=${process.env.API_KEY_YOUTUBE}`;
+  try {
+    const response = await axios.get(url);
+    const categories = response.data.items;
+    for (const category of categories) {
+      await prismadb.category.createMany({
+        data: {
+          id: category.id,
+          title: category.snippet.title,
+        },
+      });
+    }
+    return NextResponse.json(response.data);
+  } catch (err) {
+    console.log(err);
+  }
+}
