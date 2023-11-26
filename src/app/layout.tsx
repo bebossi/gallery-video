718f24464af3b86e5003d9e0f6d55e1004f3a060
bebossi <bernardobossi77@gmail.com>
2023-11-26 15:39:23 -0300

    add filter modal to the root layout, and update css when the sidebar is open

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
+'use client';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Navbar from '../Components/Navbar/Navbar';
+import clsx from 'clsx';
+import useSidebar from '../Hooks/useSideBar';
+import { usePathname } from 'next/navigation';
+import Sidebar from '../Components/Sidebar/Sidebar';
+import FilterModal from '../Components/Filters/FilterModal';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -9,12 +15,30 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const { isOpen } = useSidebar();
+  const pathname = usePathname();
+  const isVideoPage = pathname.includes('video');
+  const isPlaylistPage = pathname.includes('playlists');
+
   return (
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        {children}
+        <Sidebar />
+        <FilterModal />
+        <div
+          className={clsx(
+            'pt-[10rem] ',
+            isOpen && !(isPlaylistPage && isVideoPage) && 'ml-[18rem]',
+            isOpen && isPlaylistPage && isVideoPage && ' opacity-20',
+          )}
+        >
+          {children}
+        </div>
       </body>
     </html>
   );
+}
+function useLayoutClasses(): { getClasses: any } {
+  throw new Error('Function not implemented.');
 }
