10710579bc61361004f593f185210eb7f6da9307
bebossi <bernardobossi77@gmail.com>
2023-11-26 15:33:48 -0300

    update navbar

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,21 +1,21 @@
 'use client';
 import Link from 'next/link';
 import Logo from './Logo';
-import UserMenu from './UserMenu';
+// import UserMenu from './UserMenu';
 import SearchBar from './SearchBar';
-import Sidebar from '../Sidebar/Sidebar';
 
 const Navbar = () => {
   return (
-    <div className=" w-full z-10 shadow-sm">
+    <div className="fixed bg-black w-full  shadow-sm">
       <div className="py-5 border-b-[0.5px]">
-        <Sidebar />
-        <div className="flex items-center justify-around gap-3 ">
-          <Link className="mx-3" href={`/`}>
-            <Logo />
-          </Link>
+        <div className="flex items-center justify-evenly  ">
+          <div>
+            <Link href={`/`}>
+              <Logo />
+            </Link>
+          </div>
           <SearchBar />
-          <UserMenu />
+          {/* <UserMenu /> */}
         </div>
       </div>
     </div>
--- src/Components/Navbar/SearchBar.tsx
+++ src/Components/Navbar/SearchBar.tsx
@@ -1,9 +1,12 @@
 'use client';
+import useFilterModal from '@/src/Hooks/useFilterModal';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
-import { LuSearch } from 'react-icons/lu';
+import { LuSearch, LuFilter } from 'react-icons/lu';
 
 const SearchBar = () => {
+  const filterModal = useFilterModal();
+
   const [searchQuery, setSearchQuery] = useState('');
   const router = useRouter();
 
@@ -22,12 +25,15 @@ const SearchBar = () => {
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
         type="text"
-        className=" h-full w-full border border-slate-200 rounded-l-full p-2  bg-black"
+        className=" h-full w-full border border-slate-200 rounded-l-full p-2  bg-black outline-none"
         placeholder="What are you looking for?"
       />
-      <button className="h-full text-white bg-black border border-l-0 border-slate-200 rounded-r-full p-1 pr-3">
+      <button className="h-full text-white bg-black border border-l-0 border-slate-200 rounded-r-full p-[0.3rem] pr-3">
         <LuSearch size={25} />
       </button>
+      <span onClick={filterModal.toggleSidebar} className="mx-[1rem]">
+        <LuFilter size={25} />
+      </span>
     </form>
   );
 };
