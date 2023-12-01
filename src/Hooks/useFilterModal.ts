8bfdb6ed38a679de111140f95b2534da040a93ff
bebossi <bernardobossi77@gmail.com>
2023-12-01 11:52:20 -0300

    feat: update hook of filter modal to have more funcionalities

--- src/Hooks/useFilterModal.ts
+++ src/Hooks/useFilterModal.ts
@@ -2,12 +2,27 @@ import { create } from 'zustand';
 
 interface UseFilterModalStore {
   isOpen: boolean;
+  uploadDate: string;
+  type: string;
+  sortBy: string;
   toggleSidebar: () => void;
+  setUploadDate: (value: string) => void;
+  setType: (value: string) => void;
+  setSortBy: (value: string) => void;
 }
 
 const useFilterModal = create<UseFilterModalStore>((set) => ({
   isOpen: false,
+  uploadDate: '',
+  type: '',
+  sortBy: '',
   toggleSidebar: () => set((state) => ({ isOpen: !state.isOpen })),
+  setUploadDate: (value) =>
+    set((state) => ({ uploadDate: state.uploadDate === value ? '' : value })),
+  setType: (value) =>
+    set((state) => ({ type: state.type === value ? '' : value })),
+  setSortBy: (value) =>
+    set((state) => ({ sortBy: state.sortBy === value ? '' : value })),
 }));
 
 export default useFilterModal;
