2b682d7b505350e7c9b89b65623a4a3a1e15e6aa
bebossi <bernardobossi77@gmail.com>
2023-12-11 10:19:01 -0300

    update css of playlist components

--- src/Components/Playlist/PlaylistInfo.tsx
+++ src/Components/Playlist/PlaylistInfo.tsx
@@ -1,16 +1,19 @@
 'use client';
-import { Channel, Playlist, Video } from '@prisma/client';
+import { Channel, Playlist, User, Video } from '@prisma/client';
 import axios from 'axios';
 import Link from 'next/link';
+import { IoIosAddCircle, IoIosRemoveCircle } from 'react-icons/io';
 
 export interface PlaylistInfoProps {
   playlist: Playlist & {
     ownerChannel: Channel;
     channels: Channel[];
     videos: Video[];
+    users: User[];
   };
+  isSaved?: boolean;
 }
-const PlaylistInfo: React.FC<PlaylistInfoProps> = ({ playlist }) => {
+const PlaylistInfo: React.FC<PlaylistInfoProps> = ({ playlist, isSaved }) => {
   const savePlaylist = async () => {
     try {
       await axios.put('/api/savePlaylist', {
@@ -22,14 +25,14 @@ const PlaylistInfo: React.FC<PlaylistInfoProps> = ({ playlist }) => {
   };
 
   return (
-    <div className="flex flex-row items-center justify-start rounded-xl lg:flex-col h-full lg:h-screen w-screen sm:w-full md:w-full lg:w-1/2 xl:w-1/3 2xl:w-1/3 bg-gradient-to-b from-slate-800 bg-opacity-80">
+    <div className="flex flex-row items-center justify-start rounded-xl lg:flex-col h-full lg:h-screen w-full sm:w-full md:w-full lg:w-1/2 xl:w-1/2 2xl:w-1/3 bg-gradient-to-b from-slate-800 bg-opacity-80">
       <Link
         className="w-full"
         href={`/playlists/${playlist.id}/${playlist.videos[0].id}`}
       >
         <div className="group relative flex flex-col items-center justify-center w-full rounded-full ">
           <img
-            className="h-40 lg:h-48 w-4/5 my-9 rounded-2xl  group-hover:opacity-20"
+            className="h-fit w-fit my-9 rounded-2xl  group-hover:opacity-20"
             src={playlist.thumbnailUrl!}
             alt={`${playlist.name} thumbnail`}
           />
@@ -39,6 +42,15 @@ const PlaylistInfo: React.FC<PlaylistInfoProps> = ({ playlist }) => {
         </div>
       </Link>
       <div className="flex flex-col justify-start items-start w-full px-6 sm:px-12">
+        {isSaved ? (
+          <button title="Remove playlist" onClick={savePlaylist}>
+            <IoIosRemoveCircle size={25} />
+          </button>
+        ) : (
+          <button title="Save playlist" onClick={savePlaylist}>
+            <IoIosAddCircle size={25} />
+          </button>
+        )}
         <h1 className="font-bold text-2xl sm:text-3xl mb-2 sm:mb-4">
           {playlist.name}
         </h1>
@@ -49,7 +61,6 @@ const PlaylistInfo: React.FC<PlaylistInfoProps> = ({ playlist }) => {
           {playlist.description || 'Playlist sem descrição'}
         </p>
       </div>
-      <button onClick={savePlaylist}>save playlist</button>
     </div>
   );
 };
