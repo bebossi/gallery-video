187b7d98384badc2d98a29b8b6145db7620134dd
bebossi <bernardobossi77@gmail.com>
2023-12-12 06:29:17 -0300

    add funcionality to save playlist

--- src/app/(pages)/playlists/[playlistId]/page.tsx
+++ src/app/(pages)/playlists/[playlistId]/page.tsx
@@ -3,6 +3,7 @@ import DisplayVideo from '@/src/Components/Videos/DisplayVideo';
 import { isPlaylistSaved } from '@/src/app/api/isPlaylistSaved/route';
 import prismadb from '@/src/lib/prismadb';
 import { currentUser } from '@clerk/nextjs/server';
+import { Video } from '@prisma/client';
 
 interface PlaylistPageProps {
   params: {
@@ -29,6 +30,7 @@ const PlaylistPage: React.FC<PlaylistPageProps> = async ({ params }) => {
   const user = await currentUser();
 
   const isSaved = await isPlaylistSaved(playlistId, user!.id);
+
   return (
     <div className="flex flex-col justify-start items-start lg:flex-row mt-6 w-[90vw]">
       <PlaylistInfo playlist={playlist!} isSaved={isSaved} />
