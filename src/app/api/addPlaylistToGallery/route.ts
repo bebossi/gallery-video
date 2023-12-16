5e772d217a151b982ec698a6ce2628fb5d7335b9
bebossi <bernardobossi77@gmail.com>
2023-12-16 07:55:35 -0300

    create route for users save and unsave galleryplaylists

---
+++ src/app/api/addPlaylistToGallery/route.ts
@@ -0,0 +1,26 @@
+import prismadb from '@/src/lib/prismadb';
+import { NextResponse } from 'next/server';
+
+export const PUT = async (req: Request) => {
+  try {
+    const { galleryId, playlistId } = await req.json();
+
+    const gallery = await prismadb.playlistGallery.update({
+      where: {
+        id: galleryId,
+      },
+      data: {
+        playlists: {
+          connect: {
+            id: playlistId,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(gallery);
+  } catch (err) {
+    console.error(err);
+    throw new Error('Something went wrong ');
+  }
+};
