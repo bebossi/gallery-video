9d5523c15cc6c9104e9dcb3c2bdea9c093b54854
bebossi <bernardobossi77@gmail.com>
2023-12-18 15:22:59 -0300

    feat: update filterOptions const to adapt according type

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,7 +3,7 @@ export const filterOptions = [
     key: 'uploadDate',
     label: 'UPLOAD DATE',
     options: [
-      { label: 'Last hour', value: 'last hour' },
+      { label: 'Last hour', value: 'hour' },
       { label: 'Today', value: 'today' },
       { label: 'This week', value: 'week' },
       { label: 'This month', value: 'month' },
@@ -22,10 +22,20 @@ export const filterOptions = [
   {
     key: 'sortBy',
     label: 'SORT BY',
-    options: [
-      { label: 'Date', value: 'date' },
-      { label: 'Views', value: 'views' },
-      { label: 'Rating', value: 'Rating' },
-    ],
+    getOptions: (selectedType: string) => {
+      if (selectedType === 'channels') {
+        return [
+          { label: 'Date', value: 'publishedAt' },
+          { label: 'Views', value: 'viewCount' },
+          { label: 'Subscribers', value: 'subscriberCount' },
+        ];
+      } else {
+        return [
+          { label: 'Date', value: 'publishedAt' },
+          { label: 'Views', value: 'viewCount' },
+          { label: 'Rating', value: 'likeCount' },
+        ];
+      }
+    },
   },
 ];
